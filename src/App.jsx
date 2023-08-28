af75995d2586fda73905dd49b6095c25e1d53f62
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2023-08-28 11:00:09 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
 
-function App() {
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
-  return <h1>Hello World!</h1>
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="João Victor Ramalho Alves"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit inventore quibusdam veritatis ratione nihil dolore perferendis repellendus debitis assumenda placeat saepe laudantium ab, nulla pariatur quasi quaerat culpa iste voluptatibus!"
+          />
+
+          <Post
+            author="Gabriel Weaver"
+            content="Hello"
+          />
+        </main>
+      </div>
+    </div>
+  )
 }
-
-export default App
